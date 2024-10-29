0cca94be114faf9fcd5ad6d7bce5d4a69a20ad2b
Alrik <alrbri0804@skola.goteborg.se>
2024-10-29 20:56:12 +0100

    adding bash script and lower high end cap to 10

---
+++ run_game.sh
@@ -0,0 +1,15 @@
+$ ./run_game.sh
+echo $USER
+mkdir "$USER"_labb
+cp *.java "$USER"_labb
+cd "$USER"_labb
+echo Running game from "$PWD"
+echo compiling...
+javac Guesser.java
+javac GuessingGame.java
+echo running...
+java GuessingGame
+echo Done!
+echo Removing class files...
+rm ./*.class
+ls ./
